bb89d1460edab9174211f635079f73ab12e7a973
mpchethan <31300013+mpchethan@users.noreply.github.com>
2022-01-26 13:20:20 +0530

    Update Test1.java

--- src/test/java/test7/Test1.java
+++ src/test/java/test7/Test1.java
@@ -7,7 +7,7 @@ public class Test1 {
 	@Test
 	public void t1()
 	{
-		System.out.println("Hello World");
+		System.out.println("Hello World   iuhiuh");
 		System.out.println("testing hello world");
 	}
 
